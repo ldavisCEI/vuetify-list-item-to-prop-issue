3142f01f56f9794c4688f9af272fdfd5615a3d25
Landon <ldavis@carr-engineering.com>
2024-08-26 21:44:31 -0400

    error does not happen when using RouterLink instead

---
+++ src/components/RouterLinksView.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-container>
+    <div v-for="n in 1000" :key="n">
+      <RouterLink to="/helloworld">Item {{ n }}</RouterLink>
+    </div>
+  </v-container>
+</template>
--- src/plugins/router.js
+++ src/plugins/router.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import HomeView from "@/components/HomeView.vue";
 import HelloWorld from '@/components/HelloWorld.vue';
+import RouterLinksView from '@/components/RouterLinksView.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", component: HomeView },
-    { path: "/helloWorld", component: HelloWorld }
+    { path: "/helloWorld", component: HelloWorld },
+    { path: "/routerlinks", component: RouterLinksView }
   ],
 })
 
